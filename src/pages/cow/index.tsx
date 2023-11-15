8d4ff1cfc77054f5532f14c2010af9825b63ffe2
tannazma <97254921+tannazma@users.noreply.github.com>
2023-11-15 12:00:23 +0100

    create dynamic routes for cows

--- src/pages/cow/index.tsx
+++ src/pages/cow/index.tsx
@@ -9,19 +9,19 @@ const cows = [
 const CowProfile = () => {
   const router = useRouter();
   const { id } = router.query;
-  //   console.log(id);
-  //   if (isNaN(cowId)) {
-  //     // Handle the error, perhaps show a not found message or redirect
-  //     const cow = cows.find((cow) => cow.id === cowId);
-  //     if (!cow) {
-  //         return <p>Cow not found!</p>;
-  //       }
-  //     return <p>Cow not found!</p>;
-  //   }
   if (typeof id !=="string"){
     return <div>Invalid</div>
   }
   const cowId = parseInt(id, 10);
+  //   console.log(id);
+    if (isNaN(cowId)) {
+      // Handle the error, perhaps show a not found message or redirect
+      const cow = cows.find((cow) => cow.id === cowId);
+      if (!cow) {
+          return <p>Cow not found!</p>;
+        }
+      return <p>Cow not found!</p>;
+    }
   if (cowId === undefined) {
     return "Not found";
   } else {
